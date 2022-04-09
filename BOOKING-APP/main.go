1dfa474bad7e845e488650f2c23760d07a91fb6f
Khant Kyaw <68138182+khantkyaw6@users.noreply.github.com>
2022-04-09 21:48:17 +0630

    Add files via upload

---
+++ BOOKING-APP/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const conferenceTickets int = 50
+
+var remainingTickets uint = 50
+var conferenceName = "Go Conference"
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+func main() {
+
+	greetUsers()
+
+	for {
+
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			bookTicket(userTickets, firstName, lastName, email)
+			go sendTicket(userTickets, firstName, lastName, email)
+
+			firstNames := printFirstNames()
+			fmt.Printf("The first names of booking are %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid")
+			}
+			continue
+		}
+	}
+}
+
+func printFirstNames() []string {
+	firstNames := []string{}
+
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+	return firstNames
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter Your First Name: ")
+	fmt.Scanln(&firstName)
+
+	fmt.Println("Enter Your Last Name: ")
+	fmt.Scanln(&lastName)
+
+	fmt.Println("Enter Your Email: ")
+	fmt.Scanln(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scanln(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.\n You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("####################")
+	fmt.Printf("Sending tickets:\n %v \n to email address %v \n", ticket, email)
+	fmt.Println("####################")
+}
